7749f361ceea89cb799afcc03e4b0b6bc7f2a586
Rafael Sancor <rafaelsancor2003@hotmail.com>
2025-05-16 09:34:59 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+function conectar_banco() {
+    $servidor = 'localhost:3307';
+    $usuario = 'root';
+    $senha = '';
+    $banco = 'estoque';
+    
+    $conn = mysqli_connect($servidor, $usuario, $senha, $banco);
+    if (!$conn) {
+        exit("Erro na conexão: " . mysqli_connect_error());
+    }
+    return $conn;
+}
+?>
